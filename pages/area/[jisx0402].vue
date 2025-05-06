83537fd70fa9cae20d27234afb0eb6a38354e7d3
oageo <59683387+oageo@users.noreply.github.com>
2025-05-06 17:52:43 +0900

    各自治体等ページの見た目を改善

--- pages/area/[jisx0402].vue
+++ pages/area/[jisx0402].vue
@@ -1,10 +1,33 @@
 <template>
   <div>
+    <div class="hero">
+      <TopNav />
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">{{ jisx0402 }}の消防出動情報</h1>
+          <p class="subtitle">地方公共団体コード: {{ jisx0402 }}</p>
+        </div>
+      </div>
+    </div>
     <div v-if="disasters.length">
-      <div v-for="(disaster, index) in disasters" :key="index" class="disaster">
-        <p>住所: {{ disaster.address }}</p>
-        <p>出動時刻: {{ disaster.time }}</p>
-        <p>出動種別: {{ disaster.type }}</p>
+      <div class="container p-2">
+        <div class="columns is-multiline">
+          <div v-for="(disaster, index) in disasters" :key="index" class="disaster">
+            <div class="column is-3">
+              <div class="card">
+                <div class="card-header">
+                  <h2 class="card-header-title">
+                    {{ disaster.type }}
+                  </h2>
+                </div>
+                <div class="card-content">
+                  <p>住所: {{ disaster.address }}</p>
+                  <p>時刻: {{ disaster.time }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <hr>
     </div>
@@ -12,10 +35,10 @@
       <p>現在、消防出動情報はありません。</p>
       <hr>
     </div>
-    <div v-if="source.length">
+    <div v-if="source.length" class="container m-2">
       <div v-for="(src, index) in source" :key="index">
-        <p>{{ src.name }}</p>
-        <p><a :href="src.url" rel="noopener">{{ src.url }}</a></p>
+        <p>消防本部等: {{ src.name }}</p>
+        <p>ソース: <a :href="src.url" rel="noopener">{{ src.url }}</a></p>
       </div>
     </div>
     <div v-else>
@@ -33,10 +56,9 @@ import { useRoute } from 'vue-router';
 const disasters = ref([]);
 const source = ref([]);
 const route = useRoute();
+const jisx0402 = route.params.jisx0402;
 
 onMounted(async () => {
-  const jisx0402 = route.params.jisx0402;
-
   if (!jisx0402) {
     console.error('jisx0402 is undefined');
     return;
